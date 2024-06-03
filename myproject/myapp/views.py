ddcf57564672a05476f4436e8bdf6b4f0ba541e0
MD MUSA ALAM <mdmusaalam98019@gmail.com>
2024-06-03 16:11:01 +0530

    Implement TRY EXCEPT method

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,7 +6,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import viewsets
 from .models import Customers
-# from django.contrib.auth import authenticate , login , logout
+from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
+ 
  
 
 
